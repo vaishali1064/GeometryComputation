d270e62f0271a08d4bd5c188bf44aa362f1ca22f
vaishali1064 <patelvaishali164@gmail.com>
2021-06-16 22:08:41 +0530

    Check Whether two lines are equals or not

--- Geometry.java
+++ Geometry.java
@@ -18,8 +18,33 @@ public class Geometry
 				System.out.println( " Enter  Y2 : ");
 				int Y2 = point.nextInt();
 
-				double length = Math.sqrt(Math.pow((X2 - X1), 2 ) + Math.pow((Y2 - Y1) , 2));
+				Double length = Math.sqrt(Math.pow((X2 - X1), 2 ) + Math.pow((Y2 - Y1) , 2));
 				System.out.println(" the length of the line is " + length);
+				
+				
+				System.out.println( " Enter  M1 : ");
+				int M1 = point.nextInt();
+				
+				System.out.println( " Enter  N1 : ");
+				int N1 = point.nextInt();
+				
+				System.out.println( " Enter  M2 : ");
+				int M2 = point.nextInt();
+				
+				System.out.println( " Enter  N2 : ");
+				int N2 = point.nextInt();
+
+				Double length1 = Math.sqrt(Math.pow((M2 - M1), 2 ) + Math.pow((N2 - N1) , 2));
+				System.out.println(" the length of the line is " + length1);
+				
+				if(length.equals(length1))
+				{
+					System.out.println("Both lines are equal");
+				}	
+				else
+				{
+					System.out.println("Both lines are not equal");
+				}
 				
 			}	
 
